3c6f4a7b7f6c11e333795d67d81e145f7fd5668d
Marco Afonso <mafonso333@gmail.com>
2020-02-12 11:09:25 +0000

    Add webpack config and deps to process .less files

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,10 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.less$/,
+                use: ["style-loader", "css-loader", "less-loader"]
+            },
+            {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: "babel-loader"
